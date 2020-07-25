0f1a6e5e1f79749ee02690e75dc621b05be5d07f
royeradames <royeraadames@gmail.com>
2020-07-24 22:57:57 -0400

    added swtich, route, login to app

--- secreat-family-recipes/src/App.js
+++ secreat-family-recipes/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom'
 
+//components
+import Login from './components/Login'
 
 function App() {
   return (
     <div className="App">
       Hello World
+
+      <Switch>
+        <Route path='/login'>
+          <Login />
+        </Route>
+      </Switch>
     </div>
   );
 }
