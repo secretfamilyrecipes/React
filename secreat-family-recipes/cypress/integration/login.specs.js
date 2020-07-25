672700f39f55b6607fae1987ae7b49fa9191c026
royeradames <royeraadames@gmail.com>
2020-07-25 00:37:00 -0400

    login.specs.js can input username, password, check if it has, and submit the login form

--- secreat-family-recipes/cypress/integration/login.specs.js
+++ secreat-family-recipes/cypress/integration/login.specs.js
@@ -2,7 +2,18 @@
 describe('Input and submit form', () => {
     it('can navigate to the site', ()=> {
         cy.visit('http://localhost:3000/login')
-        cy.url().should('include', 'localhost')
+        cy.url().should('include', '/login')
+    })
+    it('answer login form', () => {
+        cy.get('input[name="username"]').click().type('yeswecan')
+        cy.get('input[name="password"]').click().type('untilwecannot')
+    })
+    it('check login form to see if it is fill up', () => {
+        cy.get('input[name="username"]').should('have.value','yeswecan')
+        cy.get('input[name="password"]').should('have.value', 'untilwecannot')
+    })
+    it('Submit login Form', () => {
+        cy.get('form').submit()
     })
    
 })
