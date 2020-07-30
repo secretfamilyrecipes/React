3be89629fc2e143dc34e583183a1639d89cc773b
royeradames <royeraadames@gmail.com>
2020-07-30 19:56:03 -0400

    made new RecipiesList, and working on adding a new card for recipies, and links to does card

--- secretfamilyrecipes/src/Components/DisplayRecipes.js
+++ secretfamilyrecipes/src/Components/DisplayRecipes.js
@@ -2,8 +2,9 @@ import React, { useContext, useEffect } from 'react'
 import { RecipesContext } from '../utils/RecipesContext'
 import { useHistory } from 'react-router-dom'
 import 'semantic-ui-css/semantic.min.css'
-import { Card, Icon } from 'semantic-ui-react'
 
+//component
+import RecipiesList from './RecipiesList'
 
 export default function DisplayRecipes() {
     //Shanon Fritz code
@@ -16,59 +17,16 @@ export default function DisplayRecipes() {
     //Shanon Fritz code
 
     //royer code
-    const description = [
-        <div className='description-container'>
-            <h2>Title</h2>
-            <ul>
-                <li>Test</li>
-            </ul>
-        </div>
-    ].join(' ')
 
 
     return (
-        <Card.Group centered>
-        <Card fluid color='orange' onClick='move to content page'>
-            <Card.Content>
-                <Card.Header><Icon name='book' />Title</Card.Header>
-                <Card.Description>
-                    <ul>
-                        <li>Description: </li>
-                        <li>Total time: </li>
-                        <li>Incrediants: </li>
-                    </ul>
-                </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-                <Icon name='food' />10 Steps
-            </Card.Content>
-        </Card>
-
-        </Card.Group>
+        <RecipiesList
+            recipes={recipes}
+            redirect={redirect}
+        />
         //royer code
 
-        /*
-        <div>
-            <div>
-                <button onClick={redirect}>Add a Recipe</button>
-            </div>
-            {recipes.length > 0 && (
-                <div>
-                    {recipes.map(recipe =>(
-                        <div key={recipe.id}>
-                            <h3>Title:&nbsp{recipe.recipeName}</h3>
-                            <p>Source:&nbsp{recipe.recipeSource}</p>
-                            {Number(recipe.prepTime) + Number(recipe.cookTime) > 0 && (
-                                <p>Total Time:&nbsp{Number(recipe.prepTime) + Number(recipe.cookTime)} minutes</p>
-                            )}
-                            <p>Ingredients:&nbsp{recipe.ingredients}</p>
-                            <p>Steps:&nbsp{recipe.directions}</p>
-                        </div>
-                    ))}
-                </div>
-            )}
-        </div>
-        */
+        
     )
 }
 
---
+++ secretfamilyrecipes/src/Components/RecipiesList.js
@@ -0,0 +1,92 @@
+//royer adames
+import React from 'react'
+import { Card, Icon, List } from 'semantic-ui-react'
+
+export default function RecipiesList({ recipes,  redirect}) {
+    function unpackAllRecipes() {
+        let unpackRecipes
+        const thereIsRecipies = recipes.length > 0
+        if (thereIsRecipies) {
+            unpackRecipes = recipes.map(recipe => {
+                debugger
+                 return (<Card.Group centered>
+                    <Card color='orange' onClick='move to content page'>
+                        <Card.Content>
+                            <Card.Header><Icon name='book' />Title</Card.Header>
+                            <Card.Description>
+                                
+                            </Card.Description>
+                        </Card.Content>
+                        <Card.Content extra>
+                            <Icon name='food' />10 Steps
+                        </Card.Content>
+                    </Card>
+
+                </Card.Group>)
+            })
+        }
+        return unpackRecipes
+    }
+    return (
+        <div>
+            <div>
+                <button onClick={redirect}>Add a Recipe</button>
+            </div>
+            {recipes.length > 0 && (
+                <div>
+                    {recipes.map(recipe =>(
+                        <div key={recipe.id}>
+                            <h3>Title:&nbsp{recipe.recipeName}</h3>
+                            <p>Source:&nbsp{recipe.recipeSource}</p>
+                            {Number(recipe.prepTime) + Number(recipe.cookTime) > 0 && (
+                                <p>Total Time:&nbsp{Number(recipe.prepTime) + Number(recipe.cookTime)} minutes</p>
+                            )}
+                            <p>Ingredients:&nbsp{recipe.ingredients}</p>
+                            <p>Steps:&nbsp{recipe.directions}</p>
+                        </div>
+                    ))}
+                </div>
+            )}
+        </div>
+    )
+}
+//royer adames
+
+//Shanon Fritz code
+/*
+        <div>
+            <div>
+                <button onClick={redirect}>Add a Recipe</button>
+            </div>
+            {recipes.length > 0 && (
+                <div>
+                    {recipes.map(recipe =>(
+                        <div key={recipe.id}>
+                            <h3>Title:&nbsp{recipe.recipeName}</h3>
+                            <p>Source:&nbsp{recipe.recipeSource}</p>
+                            {Number(recipe.prepTime) + Number(recipe.cookTime) > 0 && (
+                                <p>Total Time:&nbsp{Number(recipe.prepTime) + Number(recipe.cookTime)} minutes</p>
+                            )}
+                            <p>Ingredients:&nbsp{recipe.ingredients}</p>
+                            <p>Steps:&nbsp{recipe.directions}</p>
+                        </div>
+                    ))}
+                </div>
+            )}
+        </div>
+        */
+//Shanon Fritz code
+
+//royer adames
+{/* <List as='ol'>
+                                    <List.Item as='li' value='*'>
+                                        Total Time:
+                            </List.Item>
+                                    <List.Item as='li' value='*'>
+                                        Total Incrediants
+                            </List.Item>
+                                    <List.Item as='li' value='*'>
+                                        Total Steps
+                            </List.Item>
+                                </List> */}
+//royer adames
